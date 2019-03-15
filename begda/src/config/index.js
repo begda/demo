19d24e715dcd76039f71623859fa8f0950a1106d
liaohui1080 <liaohui1080@tianliaohui.com>
2019-03-15 09:38:41 +0800

    对windows路径支持

--- begda/src/config/index.js
+++ begda/src/config/index.js
@@ -9,7 +9,7 @@ const getRootDir = () => {
     // 获取文件所在的当前目录
     const isPath=path.resolve('.')
 
-    // 分割目录为数组
+    // 分割目录为数组 path.sep 是获取当前系统的路径分隔符,因为 windows \\ 和 mac / 的路径分隔符不一样
     const pathArr=isPath.split(path.sep)
 
     // 递减目录数组
